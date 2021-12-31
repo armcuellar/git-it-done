e29664b065d172e94d239c13bb8114aed5576742
Armando <cuelarmando@yahoo.com>
2021-12-31 18:40:23 -0500

    added fetch api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,13 @@
-var getUserRepos = function () {
-    console.log("function was called");
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+    // format the github api url
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the URL
+    fetch(apiURL).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
 };
 
 getUserRepos();
