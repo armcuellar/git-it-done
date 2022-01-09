f9c50e67afdcd077f72b45c47677874a8be355e0
Armando <cuelarmando@yahoo.com>
2022-01-09 18:01:11 -0500

    added message for more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 
 var issuesContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector(".limit-warning")
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,12 +10,16 @@ var getRepoIssues = function (repo) {
             response.json().then(function (data) {
                 // padd response data to dom function
                 displayIssues(data);
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
             alert("There was a problem with your request!");
         }
     })
+
 };
 
 var displayIssues = function (issues) {
@@ -52,5 +57,17 @@ var displayIssues = function (issues) {
         issuesContainerEl.appendChild(issuesEl);
     }
 };
+var displayWarning = function (repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on Github.com"
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
 
 getRepoIssues("facebook/react");
