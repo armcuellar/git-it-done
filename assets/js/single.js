95667aec9f608d274658e6ece1283e6a075fea5e
Armando <cuelarmando@yahoo.com>
2022-01-11 09:38:04 -0500

    added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,23 @@
 
 var issuesContainerEl = document.querySelector("#issues-container");
-var limitWarningEl = document.querySelector(".limit-warning")
+var limitWarningEl = document.querySelector(".limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -10,13 +27,16 @@ var getRepoIssues = function (repo) {
             response.json().then(function (data) {
                 // padd response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
                 if (response.headers.get("Link")) {
                     displayWarning(repo);
                 }
             });
         }
         else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     })
 
@@ -70,4 +90,4 @@ var displayWarning = function (repo) {
     limitWarningEl.appendChild(linkEl);
 }
 
-getRepoIssues("facebook/react");
+getRepoName();
